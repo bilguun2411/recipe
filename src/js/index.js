7157296bc2cc0215ef5f2ccadf6f1b470d785438
bilguun2411 <bilguundorj24@gmail.com>
2023-06-01 19:22:18 +0800

    Search icon gargan

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,44 @@
 require("@babel/polyfill");
 import Search from './model/search';
+import { elements, renderLoader,clearLoader } from './view/base';
+import * as searchView from './view/searchView';
 
+/**
+ * webb app in tuluv 
+ * hailtiin query
+ * tuhain uzuulj bgaa
+ * laked recipes
+ * zahialj bgaa joriin nairlaguud
+ */
 
+const state = {};
 
-let search  = new Search("pasta");
+const controlSearch = async () => {
+    
+    //1.eb s hailtiin tlhuur ug avna
+    const query = searchView.getInput();
+    
+    if(query){
+         // 2) Шинээр хайлтын обьектийг үүсгэж өгнө.
+        state.search = new Search(query);
 
-search.getResult().then(r=>console.log(r));
+        // 3) Хайлт хийхэд зориулж дэлгэцийг UI бэлтгэнэ.
+        searchView.clearSearchQuery();
+        searchView.clearSearchResult();
+        renderLoader(elements.searchResulstDiv);
+        // 4) Хайлтыг гүйцэтгэнэ
+        await state.search.getResult();
+       
+        // 5) Хайлтын үр дүнг дэлгэцэнд үзүүлнэ.
+        clearLoader();
+        if(state.search.result === undefined ) alert("bhgui")
+        else searchView.renderRecipes(state.search.result);
+        //console.log(state.search.result);
+    }
+};
+
+elements.searchFrom.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+} );
 
--- src/js/model/search.js
+++ src/js/model/search.js
@@ -9,12 +9,12 @@ export default class Search{
 
     async  getResult() {
         try {
-            let res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`);
-            this.result = res.data.recipes;
+            let result = await axios('https://forkify-api.herokuapp.com/api/search?q='+this.query);
+            this.result = result.data.recipes;
             return this.result
         
         }catch(error){
-            alert(error);
+            console.log(error);
         }
         
     }
